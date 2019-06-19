47cee02871c35d04e3e68f1ba112e2da64b74e6f
David Byers <byersdz@gmail.com>
2019-06-18 20:24:19 -0400

    resize code editor on resize and panel changes

--- src/containers/CodeEditor/CodeEditor.js
+++ src/containers/CodeEditor/CodeEditor.js
@@ -17,6 +17,66 @@ class CodeEditor extends React.Component {
     super( props );
 
     this.editor = null;
+    this.containerRef = React.createRef();
+
+    this.state = {
+      containerWidth: 0,
+      containerHeight: 0,
+    };
+
+    this.updateDimensions = this.updateDimensions.bind( this );
+  }
+
+  componentDidMount() {
+    window.addEventListener( 'resize', this.updateDimensions );
+
+    this.updateDimensions();
+  }
+
+  componentDidUpdate( prevProps ) {
+    const {
+      navigationPanelIsOpen,
+      referencePanelIsOpen,
+    } = this.props;
+    const {
+      navigationPanelIsOpen: prevNavIsOpen,
+      referencePanelIsOpen: prevRefIsOpen,
+    } = prevProps;
+
+    if (
+      navigationPanelIsOpen !== prevNavIsOpen
+      || referencePanelIsOpen !== prevRefIsOpen
+    ) {
+      this.updateDimensions();
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener( 'resize', this.updateDimensions );
+  }
+
+  updateDimensions() {
+    const { navigationPanelIsOpen, referencePanelIsOpen } = this.props;
+
+    const windowWidth = window.innerWidth;
+
+    let referencePanelWidth = 20;
+    if ( referencePanelIsOpen ) {
+      if ( windowWidth <= 1700 ) {
+        referencePanelWidth = 384;
+      }
+      else {
+        referencePanelWidth = 512;
+      }
+    }
+
+    const navigationPanelWidth = navigationPanelIsOpen ? 200 : 40;
+
+    const containerWidth = windowWidth - referencePanelWidth - navigationPanelWidth;
+    this.setState( {
+      containerWidth,
+      containerHeight: this.containerRef.current.offsetHeight,
+    } );
   }
 
   handleEditorLoad( editor ) {
@@ -42,23 +102,45 @@ class CodeEditor extends React.Component {
 
   render() {
     const { script } = this.props;
+    const { containerWidth, containerHeight } = this.state;
+    console.log( containerWidth );
+    const maxEditorWidth = 1200;
+    const minEditorWidth = 720;
+
+    let editorWidth = containerWidth - 32;
+
+    if ( editorWidth > maxEditorWidth ) {
+      editorWidth = maxEditorWidth;
+    }
+
+    if ( editorWidth < minEditorWidth ) {
+      editorWidth = minEditorWidth;
+    }
+
+    console.log( editorWidth );
+
+
+    const editorHeight = containerHeight - 32;
 
     return (
       <div className="code-editor">
-        <AceEditor
-          value={ script.text }
-          mode="javascript"
-          theme="twilight"
-          name="blah1"
-          width="720px"
-          fontSize={ 14 }
-          onChange={ ( v, e ) => this.handleTextChange( v, e ) }
-          onCursorChange={ s => this.handleCursorChange( s ) }
-          onLoad={ e => this.handleEditorLoad( e ) }
-          editorProps={ { $blockScrolling: true } }
-          tabSize={ 2 }
-          focus
-        />
+        <div className="editor-container" ref={ this.containerRef }>
+          <AceEditor
+            value={ script.text }
+            mode="javascript"
+            theme="twilight"
+            name="blah1"
+            width={ `${ editorWidth }px` }
+            height={ `${ editorHeight }px` }
+            fontSize={ 14 }
+            onChange={ ( v, e ) => this.handleTextChange( v, e ) }
+            onCursorChange={ s => this.handleCursorChange( s ) }
+            onLoad={ e => this.handleEditorLoad( e ) }
+            editorProps={ { $blockScrolling: true } }
+            tabSize={ 2 }
+            focus
+          />
+        </div>
       </div>
     );
   }
@@ -68,6 +150,8 @@ CodeEditor.propTypes = {
   script: PropTypes.object.isRequired,
   _setScript: PropTypes.func.isRequired,
   activeIndex: PropTypes.number.isRequired,
+  navigationPanelIsOpen: PropTypes.bool.isRequired,
+  referencePanelIsOpen: PropTypes.bool.isRequired,
 };
 
 function mapStateToProps( state ) {
@@ -76,6 +160,8 @@ function mapStateToProps( state ) {
   return {
     activeIndex,
     script,
+    navigationPanelIsOpen: state.layout.navigationPanelIsOpen,
+    referencePanelIsOpen: state.layout.referencePanelIsOpen,
   };
 }
 
