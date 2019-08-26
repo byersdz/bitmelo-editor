62b90f9064a1371d481089f2bb1f47552d8881a3
David Byers <byersdz@gmail.com>
2019-08-26 19:53:04 -0400

    work on about page

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -41,19 +41,92 @@ class About extends React.Component {
           }
         </div>
         <h2 className="welcome">Welcome to Bitmelo!</h2>
-        <p>Bitmelo is a game engine and editor for making pixel art games.</p>
+        <p>
+          {
+            `
+            Bitmelo is a game editor and engine for making small pixel art games.
+            This editor has everything you need to develop a game in one convienient location.
+            You can code, draw tiles and tilemaps, design sound effects, view the documentation,
+            and playtest your game, all without having to break focus by switching applications.
+            Once complete you can then export your game as a single HTML file.
+            `
+          }
+
+        </p>
+        <p>
+          {
+            `
+            Bitmelo is currently in early access. Right is has everything you need to make a minimal game,
+            however many more features are planned for the future.
+            `
+          }
+        </p>
         { forSaleRender }
         <h2>Community</h2>
         <p>
           {
             `
-            Hi, my name's David and I'm the developer of Bitmelo!
-            You can check out my Twitter @DavidZByers to get development updates.
+            Hi, my name's David and I'm the developer of Bitmelo! To get development
+            updates you can check out my Twitter
+            `
+          }
+          <AButton href="https://twitter.com/DavidZByers">
+            {
+              '@DavidZByers.'
+            }
+          </AButton>
+        </p>
+        <p>
+          {
+            `
+            Check out the official Discord server
+            `
+          }
+          <AButton href="https://discord.gg/akar4FG">
+            {
+              'here.'
+            }
+          </AButton>
+        </p>
+        <p>
+          {
+            `
+            Or the official Reddit
+            `
+          }
+          <AButton href="https://www.reddit.com/r/bitmelo/">
+            {
+              'here.'
+            }
+          </AButton>
+        </p>
+        <h2>Is Bitmelo Open Source?</h2>
+        <p>
+          {
+            `
+              The Bitmelo engine is open source under the MIT License. This means that you
+              are free to do whatever you want with the games you make using the engine,
+              including games made in the editor.
             `
           }
         </p>
-        <h2>Is Bitmelo Open Source?</h2>
-
+        <AButton href="https://github.com/byersdz/bitmelo">
+          View the Bitmelo engine source on Github
+        </AButton>
+        <p>
+          {
+            `
+              The Bitmelo editor is licensed using a source available license. This license
+              allows you to download the source for personal use, but does not allow you to
+              distribute it to third parties. In addition, three years after the
+              release of each version of the editor, that version is dual licensed to
+              use the GNU Affero General Public License.
+            `
+          }
+        </p>
+        <AButton href="https://github.com/byersdz/bitmelo-editor">
+          View the Bitmelo editor source on Github
+        </AButton>
       </div>
     );
   }
