af3935b4a1a0e76f3834e4d4e4ca96ead44d663e
David Byers <byersdz@gmail.com>
2019-08-31 15:08:59 -0400

    force navigation tab closed on small screens

--- src/containers/NavigationTab/NavigationTab.js
+++ src/containers/NavigationTab/NavigationTab.js
@@ -20,13 +20,33 @@ import {
 import { toggleNavigationPanel } from 'State/Layout/navigationPanelIsOpen';
 import { STOP_ALL_AUDIO, addAudioEvent } from 'State/Sound/audioEvents';
 
+import { useExtraSmallWidth } from 'Style/dimensions';
+
 import './NavigationTab.scss';
 
 class NavigationTab extends React.Component {
   constructor( props ) {
     super( props );
 
+    this.state = {
+      windowWidth: 10000,
+    };
+
     this.handleClick = this.handleClick.bind( this );
+    this.updateDimensions = this.updateDimensions.bind( this );
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener( 'resize', this.updateDimensions );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener( 'resize', this.updateDimensions );
+  }
+
+  updateDimensions() {
+    this.setState( { windowWidth: window.innerWidth } );
   }
 
   handleClick( key ) {
@@ -37,6 +57,7 @@ class NavigationTab extends React.Component {
 
   render() {
     const { activeTab, isOpen, _toggleNavigationPanel } = this.props;
+    const { windowWidth } = this.state;
 
     const buttonList = [
       {
@@ -76,17 +97,26 @@ class NavigationTab extends React.Component {
       },
     ];
 
-    const className = isOpen ? 'navigation-tab open' : 'navigation-tab';
-    const hideTitles = !isOpen;
+    const forceSmall = windowWidth <= useExtraSmallWidth;
+
+    const className = isOpen && !forceSmall ? 'navigation-tab open' : 'navigation-tab';
+    const hideTitles = !isOpen || forceSmall;
+
+    const hamburgerButtonRender = !forceSmall ? (
+      <Button
+        icon="hamburger"
+        title="Toggle Navigation Panel"
+        className="toggle-panel"
+        click={ () => _toggleNavigationPanel() }
+        hideTitle
+      />
+    ) : (
+      <div className="missing-hamburger" />
+    );
+
     return (
       <div className={ className }>
-        <Button
-          icon="hamburger"
-          title="Toggle Navigation Panel"
-          className="toggle-panel"
-          click={ () => _toggleNavigationPanel() }
-          hideTitle
-        />
+        { hamburgerButtonRender }
         <ButtonTabs
           buttonList={ buttonList }
           activeButton={ activeTab }
