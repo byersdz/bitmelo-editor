b6b46b6c50c105e9fb4b1b8db28105d79f964b78
David Byers <byersdz@gmail.com>
2019-09-27 21:34:30 -0400

    Added preserve transparency option to selections

--- src/utils/gridHelpers.js
+++ src/utils/gridHelpers.js
@@ -1,12 +1,18 @@
 import cloneDeep from 'lodash.clonedeep';
 
 // add a source grid to a destination grid
-export function combineGrids( source, destination ) {
+export function combineGrids( source, destination, preserveTransparency = false ) {
   const result = cloneDeep( destination.data );
 
   for ( let y = 0; y < source.height; y += 1 ) {
     for ( let x = 0; x < source.width; x += 1 ) {
       const sourceItem = source.data[y * source.width + x];
+
+      // igonre transparent pixels
+      if ( !preserveTransparency && !sourceItem ) {
+        continue;
+      }
+
       const destX = x + source.offsetX;
       const destY = y + source.offsetY;
       if (
