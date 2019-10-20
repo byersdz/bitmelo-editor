7c148b7a310aa54e252c9758ca1dfdf9a8eb7c3b
David Byers <byersdz@gmail.com>
2019-10-19 21:59:14 -0400

    Added deselect button

--- src/state/Tileset/actions.js
+++ src/state/Tileset/actions.js
@@ -1,4 +1,6 @@
 
+import cloneDeep from 'lodash.clonedeep';
+
 export const CREATE_TILESET_EDITOR_SELECTION = 'CREATE_TILESET_EDITOR_SELECTION';
 export const APPLY_TILESET_EDITOR_SELECTION = 'APPLY_TILESET_EDITOR_SELECTION';
 export const REPOSITION_TILESET_EDITOR_SELECTION = 'REPOSITION_TILESET_EDITOR_SELECTION';
@@ -44,4 +46,21 @@ export function repositionTilesetEditorSelection(
       newEditorSelection,
     },
   };
+}
+
+export function deselectTilesetEditorSelection( tilesetState, tileSize ) {
+  const tilesetIndex = tilesetState.activeIndex;
+  const tileset = tilesetState.tilesets[tilesetIndex];
+  const layerIndex = tileset.activeLayer;
+
+  const selection = {
+    tileSize,
+    selectedTile: tileset.selectedTile,
+    selectionWidth: tileset.selectionWidth,
+    selectionHeight: tileset.selectionHeight,
+  };
+
+  const editorSelection = cloneDeep( tilesetState.editorSelection );
+
+  return applyTilesetEditorSelection( tilesetIndex, layerIndex, selection, editorSelection );
 }
