e3cb7fb01a7a79084d37f9dfb13d78f6d590266e
David Byers <byersdz@gmail.com>
2020-04-26 22:24:57 -0400

    start out on the editor page

--- src/state/Layout/activePage.js
+++ src/state/Layout/activePage.js
@@ -7,7 +7,7 @@ export const PROJECTS_PAGE = 'PROJECTS_PAGE';
 export const SELECT_ACTIVE_PAGE = 'SELECT_ACTIVE_PAGE';
 
 // Reducer
-export default function reducer( state = PROJECTS_PAGE, action ) {
+export default function reducer( state = EDITOR_PAGE, action ) {
   switch ( action.type ) {
     case SELECT_ACTIVE_PAGE: {
       return action.payload;
