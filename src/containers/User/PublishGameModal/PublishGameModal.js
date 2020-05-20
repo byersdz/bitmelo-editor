9bd767ec1fc352d266fdf02192f2be36e46e9005
David Byers <byersdz@gmail.com>
2020-05-20 06:40:30 -0400

    Add project name input and button to publish modal

--- src/containers/User/PublishGameModal/PublishGameModal.js
+++ src/containers/User/PublishGameModal/PublishGameModal.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import AccountModal from '../../../components/Account/AccountModal/AccountModal';
+import AccountTextInput from '../../../components/Account/AccountTextInput/AccountTextInput';
+import Button from '../../../components/Button/Button';
+
+import { setProjectName } from '../../../state/Project/name';
+
+import './PublishGameModal.scss';
 
 class PublishGameModal extends React.Component {
   render() {
-    const { onClose } = this.props;
+    const { onClose, projectName, _setProjectName } = this.props;
 
     return (
       <AccountModal
-        title="Publish Game"
+        title="Publish Your Game"
         className="publish-game-modal"
         onClose={ onClose }
       >
-        Publish Game Modal
+        <AccountTextInput
+          title="Project Name"
+          value={ projectName }
+          onValueChange={ v => _setProjectName( v ) }
+        />
+        <Button
+          title="Publish"
+          click={ () => console.log( 'publish' ) }
+          account
+        />
       </AccountModal>
     );
   }
@@ -21,6 +38,20 @@ class PublishGameModal extends React.Component {
 
 PublishGameModal.propTypes = {
   onClose: PropTypes.func.isRequired,
+  projectName: PropTypes.string.isRequired,
+  _setProjectName: PropTypes.func.isRequired,
 };
 
-export default PublishGameModal;
+function mapStateToProps( state ) {
+  return {
+    projectName: state.project.name,
+  };
+}
+
+function mapDispatchToProps( dispatch ) {
+  return bindActionCreators( {
+    _setProjectName: setProjectName,
+  }, dispatch );
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( PublishGameModal );
