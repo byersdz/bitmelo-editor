6a08e97b5e9d753647541406056b24d09ea304b2
David Byers <byersdz@gmail.com>
2020-05-30 20:03:39 -0400

    save user and project to local storage sepatately

--- src/utils/Saving/localStorage.js
+++ src/utils/Saving/localStorage.js
@@ -4,10 +4,25 @@ import compressProjectState from '../Convert/compressProjectState';
 export const loadStateFromLocalStorage = () => {
   try {
     const state = localStorage.getItem( 'state' );
+    const currentUser = localStorage.getItem( 'currentUser' );
+    const currentProject = localStorage.getItem( 'currentProject' );
+
     if ( state === null ) {
       return undefined;
     }
-    return JSON.parse( state );
+
+    const stateData = JSON.parse( state );
+    stateData.user = {};
+
+    if ( currentUser ) {
+      stateData.user.currentUser = JSON.parse( currentUser );
+    }
+
+    if ( currentProject ) {
+      stateData.user.currentProject = JSON.parse( currentProject );
+    }
+
+    return stateData;
   }
   catch ( e ) {
     return undefined;
@@ -17,8 +32,17 @@ export const loadStateFromLocalStorage = () => {
 export const saveStateToLocalStorage = state => {
   try {
     const savedState = compressProjectState( state );
+
+    const currentUser = savedState.user.currentUser;
+    const currentProject = savedState.user.currentProject;
+
+    delete savedState.user;
+    delete currentProject.publishedGame;
+
     const stateString = JSON.stringify( savedState );
     localStorage.setItem( 'state', stateString );
+    localStorage.setItem( 'currentUser', JSON.stringify( currentUser ) );
+    localStorage.setItem( 'currentProject', JSON.stringify( currentProject ) );
   }
   catch ( e ) {
     // ignore errors
