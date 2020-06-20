87547953783cf2bbd68105ad6900acdec505c2dd
David Byers <byersdz@gmail.com>
2020-06-19 20:08:13 -0400

    added modal count to Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,58 +2,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import Button from '../Button/Button';
 
+import { increaseModalCount, decreaseModalCount } from '../../state/Layout/modalCount';
+
 import './Modal.scss';
 
-const Modal = props => {
-  const {
-    children,
-    className,
-    showHeader,
-    title,
-    onClose,
-    noSizing,
-  } = props;
+class Modal extends React.Component {
+  componentDidMount() {
+    const { _increaseModalCount } = this.props;
+    _increaseModalCount();
+  }
 
-  let contentClassName = 'modal-content-container';
-  if ( className ) {
-    contentClassName += ` ${ className }`;
+  componentWillUnmount() {
+    const { _decreaseModalCount } = this.props;
+    _decreaseModalCount();
   }
-  if ( noSizing ) {
-    contentClassName += ' no-sizing';
-  }
-  const headerRender = showHeader ? (
-    <div className="modal-header">
-      <div className="title">
-        { title }
+
+  render() {
+    const {
+      children,
+      className,
+      showHeader,
+      title,
+      onClose,
+      noSizing,
+    } = this.props;
+
+    let contentClassName = 'modal-content-container';
+    if ( className ) {
+      contentClassName += ` ${ className }`;
+    }
+    if ( noSizing ) {
+      contentClassName += ' no-sizing';
+    }
+    const headerRender = showHeader ? (
+      <div className="modal-header">
+        <div className="title">
+          { title }
+        </div>
+        <Button
+          title="Close"
+          icon="x"
+          hideTitle
+          click={ onClose }
+        />
       </div>
-      <Button
-        title="Close"
-        icon="x"
-        hideTitle
-        click={ onClose }
-      />
-    </div>
-  ) : null;
+    ) : null;
 
-  const render = (
-    <div className="modal-container">
-      <div className={ contentClassName }>
-        { headerRender}
-        <div className="modal-content">
-          { children }
+    const mainRender = (
+      <div className="modal-container">
+        <div className={ contentClassName }>
+          { headerRender}
+          <div className="modal-content">
+            { children }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 
-  return ReactDOM.createPortal(
-    render,
-    document.body,
-  );
-};
+    return ReactDOM.createPortal(
+      mainRender,
+      document.body,
+    );
+  }
+}
 
 Modal.propTypes = {
   children: PropTypes.node.isRequired,
@@ -62,6 +78,8 @@ Modal.propTypes = {
   title: PropTypes.string,
   onClose: PropTypes.func,
   noSizing: PropTypes.bool,
+  _increaseModalCount: PropTypes.func.isRequired,
+  _decreaseModalCount: PropTypes.func.isRequired,
 };
 
 Modal.defaultProps = {
@@ -72,4 +90,10 @@ Modal.defaultProps = {
   noSizing: false,
 };
 
-export default Modal;
+function mapDispatchToProps( dispatch ) {
+  return bindActionCreators( {
+    _increaseModalCount: increaseModalCount,
+    _decreaseModalCount: decreaseModalCount,
+  }, dispatch );
+}
+export default connect( null, mapDispatchToProps )( Modal );
