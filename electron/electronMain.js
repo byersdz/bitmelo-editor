5d4c6b613183c4c47bcd8339f298eb4b07effb9c
David Byers <byersdz@gmail.com>
2020-09-07 17:44:45 -0400

    toggle devtools in electron

--- electron/electronMain.js
+++ electron/electronMain.js
@@ -1,10 +1,32 @@
 
-const { app, BrowserWindow, shell } = require( 'electron' );
+const {
+  app,
+  BrowserWindow,
+  shell,
+  Menu,
+} = require( 'electron' );
 const isDev = require( 'electron-is-dev' );
 
 function createWindow() {
   const win = new BrowserWindow( { width: 1360, height: 720, show: false } );
-  win.setMenu( null );
+
+  // eslint-disable-next-line new-cap
+  const menu = new Menu.buildFromTemplate( [
+    {
+      label: 'Window',
+      submenu: [
+        {
+          label: 'Toggle Dev Tools',
+          click: () => {
+            win.toggleDevTools();
+          },
+          accelerator: 'F12',
+        },
+      ],
+    },
+  ] );
+
+  win.setMenu( menu );
 
   if ( isDev ) {
     win.loadURL( 'http://localhost:9001' );
