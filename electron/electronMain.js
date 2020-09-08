36675bd95f4e27a4be963d14910dd9e411a55536
David Byers <byersdz@gmail.com>
2020-09-08 19:37:37 -0400

    api is now working in electron

--- electron/electronMain.js
+++ electron/electronMain.js
@@ -1,14 +1,24 @@
+/* eslint-disable dot-notation */
 
 const {
   app,
   BrowserWindow,
   shell,
   Menu,
+  session,
 } = require( 'electron' );
 const isDev = require( 'electron-is-dev' );
 
+let userTokenCookie = '';
+let refreshTokenCookie = '';
+let sessionIdCookie = '';
+
 function createWindow() {
-  const win = new BrowserWindow( { width: 1360, height: 720, show: false } );
+  const win = new BrowserWindow( {
+    width: 1360,
+    height: 720,
+    show: false,
+  } );
 
   // eslint-disable-next-line new-cap
   const menu = new Menu.buildFromTemplate( [
@@ -43,6 +53,64 @@ function createWindow() {
   win.webContents.on( 'new-window', ( event, url ) => {
     event.preventDefault();
     shell.openExternal( url );
+  } );
+
+  // change origin headers on web requests
+  const bitmeloApifilter = {
+    urls: ['*://api.bitmelo.com/*'],
+  };
+
+  session.defaultSession.webRequest.onBeforeSendHeaders( bitmeloApifilter, ( details, callback ) => {
+    console.log( 'before headers' );
+    console.log( details );
+    const originUrl = 'https://bitmelo.com';
+    // eslint-disable-next-line no-param-reassign
+    details.requestHeaders.Origin = originUrl;
+    if ( userTokenCookie || refreshTokenCookie || sessionIdCookie ) {
+      let cookieString = '';
+      if ( userTokenCookie ) {
+        cookieString += userTokenCookie;
+        cookieString += ';';
+      }
+
+      if ( refreshTokenCookie ) {
+        cookieString += refreshTokenCookie;
+        cookieString += ';';
+      }
+
+      if ( sessionIdCookie ) {
+        cookieString += sessionIdCookie;
+      }
+
+      // eslint-disable-next-line no-param-reassign
+      details.requestHeaders['Cookie'] = cookieString;
+    }
+
+    callback( { requestHeaders: details.requestHeaders } );
+  } );
+
+  // get bitmelo cookies from completed requests
+  session.defaultSession.webRequest.onCompleted( bitmeloApifilter, details => {
+    if ( details.responseHeaders['Set-Cookie'] ) {
+      console.log( 'complete' );
+      for ( let i = 0; i < details.responseHeaders['Set-Cookie'].length; i += 1 ) {
+        const cookie = details.responseHeaders['Set-Cookie'][i];
+        if ( cookie.startsWith( 'user_token' ) ) {
+          const split = cookie.split( ';' );
+          userTokenCookie = split[0];
+        }
+        else if ( cookie.startsWith( 'refresh_token' ) ) {
+          const split = cookie.split( ';' );
+          refreshTokenCookie = split[0];
+        }
+        else if ( cookie.startsWith( 'session_id' ) ) {
+          const split = cookie.split( ';' );
+          sessionIdCookie = split[0];
+        }
+      }
+
+      // save the cookies to a file
+    }
   } );
 }
 
