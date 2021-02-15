5e9c74d40b214332a090405e8de659ad53477acb
David Byers <byersdz@gmail.com>
2021-02-15 18:16:21 -0500

    changed more colors

--- src/style/colors.js
+++ src/style/colors.js
@@ -1,9 +1,9 @@
 
-export const primaryColor = '#a976bd';
-export const primaryDarkerColor = '#633d72';
+export const primaryColor = '#72ca7e';
+export const primaryDarkerColor = '#52a15d';
 
-export const secondaryColor = '#c0c0c0';
-export const secondaryDarkColor = '#9e9e9e';
+export const secondaryColor = '#c7c7c7';
+export const secondaryDarkColor = '#b1b1b1';
 
-export const backgroundColor = '#080c14';
+export const backgroundColor = '#101012';
 export const onBackgroundColor = '#ddd';
