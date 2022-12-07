79c614a71605c49c55dba3fa8d15f8f2fed85cb3
David Byers <byersdz@gmail.com>
2022-12-06 19:44:23 -0500

    import tile flag locked state

--- src/state/Layout/tileFlagsAreLocked.js
+++ src/state/Layout/tileFlagsAreLocked.js
@@ -1,9 +1,22 @@
+import get from 'lodash/get';
+import {
+  RESET_PROJECT,
+  IMPORT_PROJECT_DATA,
+} from '../globalActions';
+
 // Actions
 export const SET_TILE_FLAGS_ARE_LOCKED = 'SET_TILE_FLAGS_ARE_LOCKED';
 
 // Reducer
 export default function reducer( state = false, action ) {
   switch ( action.type ) {
+    case RESET_PROJECT: {
+      return false;
+    }
+    case IMPORT_PROJECT_DATA: {
+      const importedState = get( action, 'payload.layout.tileFlagsAreLocked', false );
+      return importedState;
+    }
     case SET_TILE_FLAGS_ARE_LOCKED: {
       return action.payload;
     }
